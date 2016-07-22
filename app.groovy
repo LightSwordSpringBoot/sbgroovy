52bde9a07426d5d9057bad2b4d2a6b2e5ce170fc
之剑 <guangjian.cgj@alibaba-inc.com>
2016-07-22 10:34:44 +0800

    add

---
+++ app.groovy
@@ -0,0 +1,10 @@
+@Grab("thymeleaf-spring4")
+@Grab("org.webjars:jquery:2.0.3-1")
+@Controller
+class Application {
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="Groovy") String name, Model model) {
+        model.addAttribute("name", name)
+        return "greeting"
+    }
+}
